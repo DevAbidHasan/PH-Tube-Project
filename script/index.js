405e3b07e9a2f6e73669e17c2062df3efcce808a
DevAbidHasan <abidhasanplabon80@gmail.com>
2025-04-16 02:39:04 +0600

    added verified icon and added card modals

--- script/index.js
+++ script/index.js
@@ -79,13 +79,15 @@ const displayVideos=(videos)=>{
           <div class="intro space-y-1">
             <h2 class="text-md font-semibold">${video.title}</h2>
             <p class="text-xs flex gap-x-1 text-gray-600">${video.authors[0].profile_name}
-                <img class="w-1/10" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt="verified badge">
+                
+                ${video.authors[0].verified == true ? `<img class="w-1/10" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt="verified badge">` :``}
             </p>
             <p class="text-xs text-gray-600">
                 ${video.others.views} Views
             </p>
           </div>
         </div>
+        <button onclick="loadVideoDetails('${video.video_id}')" class="btn btn-block">Show Details</button>
       </div>
         
         `
@@ -108,5 +110,49 @@ const loadCategoryVideos=(id)=>{
         displayVideos(data.category)
     });
 };
+const loadVideoDetails=(videoId)=>{
+    // console.log(videoId);
+    const url=`https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displayVideoDetails(data.video));
+}
+
+const displayVideoDetails=(video)=>{
+    console.log(video);
+    document.getElementById("video_details").showModal();
+    const detailsContainer=document.getElementById("details-container");
+    detailsContainer.innerHTML=`
+    <div class="card bg-base-600 image-full shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Video Modals" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p class="text-gray-300">
+    ${video.description}
+    </p>
+    <div class="mt-10 flex gap-x-3">
+    <img class="w-[37px] object-cover rounded-full" src="${video.authors[0].profile_picture}">
+    <div>
+    <p class="font-bold">
+    ${video.authors[0].profile_name} 
+    </p>
+    <p class="text-xs mt-0.5">
+    ${video.others.views} Views
+    </p>
+    </div>
+    <div>
+    ${video.authors[0].verified == true ? `<img class="w-1/5 mt-0.5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt="verified badge">` :``}</div>
+    </div>
+    <div class="card-actions justify-end">
+    
+    </div>
+  </div>
+</div>
+    `
+}
 
 loadCategories();
